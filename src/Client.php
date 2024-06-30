a90d6bc55d9bd99588e8eb296e52ab1458752962
Pulkit Jalan <pulkit1990@gmail.com>
2024-06-30 21:39:11 +0100

    chore: improve cleanup handling

--- src/Client.php
+++ src/Client.php
@@ -92,11 +92,22 @@ class Client
 
         $service = 'Google\\Service\\'.ucfirst(str_replace('_', '', $service));
 
-        if (class_exists($service)) {
-            return new $service($this->client);
+        try {
+            if (class_exists($service)) {
+                return new $service($this->client);
+            }
+        // catch any errors thrown when fetching the service
+        // this can be caused when the service was removed
+        // but the reference still exists in the autoloader
+        } catch (\ErrorException $e) {
+            if (str_contains($e->getMessage(), 'No such file or directory')) {
+                UnknownServiceException::throwForService($service, 0, $e);
+            }
+
+            throw $e;
         }
 
-        throw new UnknownServiceException($service);
+        UnknownServiceException::throwForService($service);
     }
 
     /**
